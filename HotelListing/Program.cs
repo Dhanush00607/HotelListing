27aa940d00c81e7c180f8c0f9f3ab6ccf61f5ce2
Dhanush00607 <dhanush.clg007@gmail.com>
2025-06-21 13:46:06 +0530

    Add project files.

---
+++ HotelListing/Program.cs
@@ -0,0 +1,37 @@
+using Serilog;
+using Serilog.Events;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.File(path: "D:\\feem\\STUDY MATERIAL\\HotelListing\\HotelListing\\log\\log-.txt",
+            outputTemplate: "{Timestamp: yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:1j}{NewLine} {Exception)",
+            rollingInterval: RollingInterval.Day,
+            restrictedToMinimumLevel: LogEventLevel.Information
+            ).CreateLogger();
+        try
+        {
+            Log.Information("Application is Starting");
+            CreateHostBuilder(args).Build().Run();
+        }
+        catch (Exception ex)
+        {
+            Log.Fatal(ex, "Application failed to start");
+        }
+        finally
+        {
+            Log.CloseAndFlush();
+        }
+       
+        
+    }
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+        .UseSerilog()
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>(); // This tells .NET to use your Startup class
+            });
+}
---
+++ HotelListing/Startup.cs
@@ -0,0 +1,36 @@
+﻿public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        // Register services here, like controllers
+        services.AddSwaggerGen(); // If using Startup.cs
+
+        services.AddControllers();
+        services.AddCors(o => {
+            o.AddPolicy("Allow All", builder =>
+            builder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+        });
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
+        // Set up the HTTP request pipeline
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseRouting();
+        app.UseCors("Allow All");
+        app.UseAuthorization();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
